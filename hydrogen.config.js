44a827566203a6b7f87ae699c2b823ef09352243
asumanwebdev <asumanwebdev@gmail.com>
2022-08-04 20:23:03 +0300

    add home section

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,8 +2,10 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'asuman-emporium.myshopify.com',
+    storefrontToken: '2081c5b74106b34effed85fcd7b8ebfd',
     storefrontApiVersion: '2022-07',
   },
 });
+
+//3b580e70970c4528da70c98e097c2fa0
